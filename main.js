0e6c40a04b52f4e68944111647d76926bc3a5d7a
Chaitanyacodes <chaitanya.chanekar2292@gmail.com>
2021-08-06 01:18:39 +0530

    feat: answer-display-toggle

---
+++ main.js
@@ -0,0 +1,15 @@
+const dropDownArrows = document.querySelectorAll(".arrow");
+const faqQuestions = document.querySelectorAll(".faq-question");
+const faqAnswers = document.querySelectorAll(".faq-answer");
+
+dropDownArrows.forEach((arrow, index) => {
+    arrow.addEventListener('click', () => {
+        faqQuestions[index].classList.toggle("faq-question-bold");
+        faqAnswers[index].classList.toggle("faq-answer-show");
+    })
+})
+
+
+function toggleAnswerDisplay() {
+
+}
